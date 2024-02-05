457c6af71209240d543df56a82bdf1d715e038c2
marvin.loth <marvin.loth@stud.th-luebeck.de>
2024-02-05 22:14:28 +0100

    Added comments

--- src/main/java/com/example/videoplayer/HelloApplication.java
+++ src/main/java/com/example/videoplayer/HelloApplication.java
@@ -4,25 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 
+/**
+ * This class described the Java FX Application, hence the inheritance
+ */
 public class HelloApplication extends Application {
+    /**
+     * This method defines the properties of the default stage and its content
+     * @param stage Reference to the default stage. Automatically provided by Java FX
+     * @throws IOException if an error occurred while loading the fxml file
+     */
     @Override
     public void start(Stage stage) throws IOException {
+        // Load scene
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+
+        // Set properties
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        // Show the stage
         stage.show();
     }
 
+    /**
+     * Starts the application
+     * @param args arguments at call
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/com/example/videoplayer/HelloController.java
+++ src/main/java/com/example/videoplayer/HelloController.java
@@ -2,7 +2,6 @@ package com.example.videoplayer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -11,22 +10,34 @@ import javafx.scene.media.MediaView;
 import java.io.File;
 import java.net.URI;
 
+/**
+ * This class describes how the program interacts with the view
+ */
 public class HelloController {
+    /** Reference to the root panel. Automatically initialized by Java FX*/
     @FXML
     Pane root;
+    /** Reference to the play / pause button. Automatically initialized by Java FX */
     @FXML
     Button playPause;
 
+    /** Reference to the {@link MediaPlayer} */
     MediaPlayer player;
 
+    /**
+     * This method is called whenever the {@link javafx.scene.Scene} is loaded.
+     */
     @FXML
     protected void initialize() {
+        // Load video and get URI
         File pathToMedia = new File("C:\\Users\\mloth\\Desktop\\Test.mp4");
         URI locator = pathToMedia.toURI();
 
+        // Create Media resource and MediaPlayer
         Media media = new Media(locator.toString());
         player = new MediaPlayer(media);
 
+        // Create MediaPlayer
         MediaView view = new MediaView(player);
         view.setX(100);
         view.setY(50);
@@ -35,12 +46,17 @@ public class HelloController {
         root.getChildren().add(view);
     }
 
+    /**
+     * This method is called whenever the pause / play button is clicked
+     */
     public void togglePlayPause() {
         switch (playPause.getText()) {
+            // Toggle Play --> Pause
             case "Play" -> {
                 player.play();
                 playPause.setText("Pause");
             }
+            // Toggle Pause --> Play
             case "Pause" -> {
                 player.pause();
                 playPause.setText("Play");
